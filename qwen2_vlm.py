08a9e34869d4f8b6769d90801a25437351d56271
lyteen <lyteen4@gmail.com>
2025-06-12 14:17:44 +0800

    User template

---
+++ qwen2_vlm.py
@@ -0,0 +1,70 @@
+from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
+from transformers import CLIPVisionModel
+from qwen_vl_utils import process_vision_info
+import src.tools.debug as debug
+
+logger = debug.setup_logger(mode='a')
+
+# default: Load the model on the available device(s)
+model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+    "C:/Users/Mr.chen/DL/Assets/model/qwen2_5_vl", torch_dtype="auto", device_map="auto"
+)
+
+
+
+# We recommend enabling flash_attention_2 for better acceleration and memory saving, especially in multi-image and video scenarios.
+# model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+#     "Qwen/Qwen2.5-VL-7B-Instruct",
+#     torch_dtype=torch.bfloat16,
+#     attn_implementation="flash_attention_2",
+#     device_map="auto",
+# )
+
+# default processor
+processor = AutoProcessor.from_pretrained("C:/Users/Mr.chen/DL/Assets/model/qwen2_5_vl")
+
+# The default range for the number of visual tokens per image in the model is 4-16384.
+# You can set min_pixels and max_pixels according to your needs, such as a token range of 256-1280, to balance performance and cost.
+# min_pixels = 256*28*28
+# max_pixels = 1280*28*28
+# processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "image",
+                "image": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg",
+            },
+            {"type": "text", "text": "Describe this image."},
+        ],
+    }
+]
+
+# Preparation for inference
+text = processor.apply_chat_template(
+    messages, tokenize=False, add_generation_prompt=True
+)
+image_inputs, video_inputs = process_vision_info(messages)
+logger.info(f"image_input: {image_inputs}")
+
+inputs = processor(
+    text=[text],
+    images=image_inputs,
+    videos=video_inputs,
+    padding=True,
+    return_tensors="pt",
+)
+logger.info(f"input: {inputs}")
+
+inputs = inputs.to(model.device)
+
+# Inference: Generation of the output
+# generated_ids = model.generate(**inputs, max_new_tokens=128)
+# generated_ids_trimmed = [
+#     out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+# ]
+# output_text = processor.batch_decode(
+#     generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+# )
+# print(output_text)
